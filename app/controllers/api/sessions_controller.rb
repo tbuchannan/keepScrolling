9119daff298005a217d72c9c96d704a211703b11
Terence Buchannan <terencebuchannan@gmail.com>
2017-04-18 17:59:15 -0400

    Correct assests folder

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    
+  end
+
+  def destroy
+
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,12 @@
+class Api::UsersController < ApplicationController
+  def index
+
+  end
+
+  def create
+
+
+  end
+
+  
+end
