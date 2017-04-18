aa346d54e97281dca05e3a27996ec878cccb173f
Terence Buchannan <terencebuchannan@gmail.com>
2017-04-18 18:50:55 -0400

    Correct bundle directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
   entry: './frontend/keepScrolling.jsx',
   output: {
-    filename: '../app/assets/javascripts/bundle.js',
+    filename: './app/assets/javascripts/bundle.js',
   },
   module: {
     loaders: [
