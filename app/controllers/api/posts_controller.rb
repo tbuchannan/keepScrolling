52245f0a9b471d779a5d2b13802263b6c4ddd747
Terence Buchannan <terencebuchannan@gmail.com>
2017-04-21 02:36:23 -0400

    remove debuggers

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,9 +1,18 @@
 class Api::PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def create
+    @post = Post.create(post_params)
+    if @post.save
+      render :show
+    else
+      render(
+      json: @post.errors.full_messages,
+      status: 401
+      )
   end
 
   def destroy
@@ -25,4 +34,13 @@ class Api::PostsController < ApplicationController
   def new
 
   end
+
+  private
+  def post_params
+    params.require(:post).permit(
+    :title, :author_id, :body, :photo_url, :link_url, :summary, :audio_url,
+    :video_url, :quote_url, :source)
+  end
+
+
 end
