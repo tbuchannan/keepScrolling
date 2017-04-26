1fc2a220f708e47efa22896d55265c3edcb0bafe
Terence Buchannan <terencebuchannan@gmail.com>
2017-04-26 16:21:32 -0400

    AWS files

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,15 +2,19 @@
 #
 # Table name: posts
 #
-#  id         :integer          not null, primary key
-#  title      :string
-#  content    :string
-#  body       :text
-#  author_id  :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  summary    :text
-#  source     :text
+#  id                 :integer          not null, primary key
+#  title              :string
+#  content            :string
+#  body               :text
+#  author_id          :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  summary            :text
+#  source             :text
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 require 'test_helper'
