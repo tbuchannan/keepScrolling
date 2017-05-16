e1c7058f46142d14db773312b669ceda0ebd509b
Terence Buchannan <terencebuchannan@gmail.com>
2017-05-16 14:34:10 -0400

    Remove comment

--- frontend/keepScrolling.jsx
+++ frontend/keepScrolling.jsx
@@ -10,7 +10,7 @@ import configureStore  from './store/store';
 // link = Post.create(author_id: User.third.id, title: "this will be on the link itself", body: "this is just a description", content_type: "", summary: "this is just a summary")
 // chat = Post.create(author_id: User.fourth.id, title: "title of the chat", body: "chat text", content_type: "txt")
 // audio = Post.create(author_id: User.fifth.id, title: "", body: " song description, def not a youtube link AUDIO ONLY", content_type: "mp3")
-// video = Post.create(author_id: User.first.id, title: "", body: "Good old Bruce, the man is a beast.", content_type: "mp4")
+
 
 window.tj = {user:{username: 'tnice01', password:'password123'}};
 window.txtPost = {post: {author_id: 14, title: "Mars", body: "Good old Bruce, the man is a beast.", content_type: "txt"}};
