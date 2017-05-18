c6f94fd61d0ea5ccdf5abd0583497e4e0e46ac59
Terence Buchannan <terencebuchannan@gmail.com>
2017-05-18 00:11:00 -0400

    add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create] do
       resources :follows, only: [:create, :destroy, :index]
+      get "potential_followers", on: :member
     end
     resources :posts
     resource :session, only: [:create, :destroy]
