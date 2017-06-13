94d2ddb12f82323a25b08178f5191b08c9ca37b4
Terence Buchannan <terencebuchannan@gmail.com>
2017-06-13 15:56:59 -0400

    Correct Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Post.create(author_id: User.third.id, title: "Winter is Coming", body: "Game of Thrones news source", content: "https://winteriscoming.net/", summary: "")
+# Post.create(author_id: User.fourth.id, title: "How I Met Your Mother Opening", body: "", content: "https://www.youtube.com/watch?v=ZPLOsabhQSM", summary: "")
+# Post.create(author_id: User.fourth.id, title: "Barneys Blog", body: "The most seductive man in all of manhattan", content: "https://www.barneystinsonblog.com/", summary: "")
 
 User.destroy_all
 Post.destroy_all
@@ -15,16 +16,7 @@ User.create(email: 'tj.buchannan@gmail.com', username: 'tnice01', password: 'pas
 User.create(email: 'gotfanboy@gmail.com', username: 'JonSnow', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/gotavatar.jpg');
 User.create(email: 'himym@gmail.com', username: 'HowIMetYourMother', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/himym-min.png');
 User.create(email: 'dwight@gmail.com', username: 'DKSchrute', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/avatar_47daeec01aae_512-min.png');
-
-
-# text = Post.create(author_id: User.first.id, title: "Mars", body: "Good old Bruce, the man is a beast.", content: "text")
-# photo = Post.create(author_id: User.last.id, body: "Should I add a description?",  content: "http://farm4.staticflickr.com/3826/12897959993_a654e00dd2.jpg")
-# quote = Post.create(author_id: User.second.id, title: "source information here?", body: "quote text will go in here", source: "source users", content: "quote")
-# link = Post.create(author_id: User.third.id, title: "this will be on the link itself", body: "this is just a description", content: "https://github.com/thfxdszunanjfgfg", summary: "this is just a summary")
-# chat = Post.create(author_id: User.fourth.id, title: "title of the chat", body: "chat text", content: "chat")
-# audio = Post.create(author_id: User.fifth.id, body: " song description, def not a youtube link AUDIO ONLY", content: "https://soundcloud.com/radio1stories/future-rnb")
-# video = Post.create(author_id: User.first.id, body: "Good old Bruce, the man is a beast.", content: "https://www.youtube.com/watch?v=S7OCzDNeENg")
-
-# link = Post.create(author_id: User.third.id, title: "Winter is Coming", body: "Game of Thrones news source", content: "http://winteriscoming.net/", summary: "")
-# link2 = Post.create(author_id: User.fourth.id, title: "How I Met Your Mother Opening", body: "", content: "https://www.youtube.com/watch?v=ZPLOsabhQSM", summary: "")
-# link3 = Post.create(author_id: User.fourth.id, title: "Barneys Blog", body: "The most seductive man in all of manhattan", content: "http://www.barneystinsonblog.com/", summary: "")
+User.create(email: 'jerome@gmail.com', username: 'Playa4rmHimalayas', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/jerome.jpg');
+User.create(email: 'teemo@gmail.com', username: 'Teemo', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/Teemo_square.png');
+User.create(email: 'ramen@gmail.com', username: 'Naruto Uzumaki', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/naruto-min.png');
+User.create(email: 'wethebest@gmail.com', username: 'DJ Khaled', password: 'password123', avatar: 'https://s3.amazonaws.com/keepscrolling-pro/khaled.jpg');
