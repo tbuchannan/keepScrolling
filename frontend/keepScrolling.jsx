9ba632e95aec445fee139554482a9fe1f694610c
Terence Buchannan <terencebuchannan@gmail.com>
2017-06-13 15:53:23 -0400

    remove window variables

--- frontend/keepScrolling.jsx
+++ frontend/keepScrolling.jsx
@@ -5,11 +5,6 @@ import * as APIUtil from './util/session_api_util';
 import * as PostUtil from './util/post_api_util';
 import configureStore  from './store/store';
 
-window.tj = {user:{username: 'tnice01', password:'password123'}};
-window.txtPost = {post: {author_id: 14, title: "Mars", body: "Good old Bruce, the man is a beast.", content_type: "txt"}};
-window.createPost = PostUtil.createPost;
-window.logout = APIUtil.logout;
-
 document.addEventListener("DOMContentLoaded", () => {
   let store;
     if (window.current_user) {
@@ -21,5 +16,4 @@ document.addEventListener("DOMContentLoaded", () => {
     window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
-
 });
