2f618b5d9ff4e50920cf6895bc4954b261aa93e4
Terence Buchannan <terencebuchannan@gmail.com>
2017-10-07 03:21:26 -0400

    Add describe block for specs

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -14,8 +14,10 @@ require 'rails_helper'
 RSpec.describe Follow, type: :model do
   subject(:follow) { FactoryGirl.build(:follow) }
 
-  it { should validate_presence_of :followee }
-  it { should validate_presence_of :follower }
+  describe 'validations' do
+    it { should validate_presence_of :followee }
+    it { should validate_presence_of :follower }
 
-  it { should validate_uniqueness_of(:follower_id).scoped_to(:followee_id) }
+    it { should validate_uniqueness_of(:follower_id).scoped_to(:followee_id) }
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -21,7 +21,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   subject(:post) { FactoryGirl.build(:post)}
+  describe 'validations' do
 
-  it { should validate_presence_of :author_id}
-  it { should belong_to :author}
+    it { should validate_presence_of :author_id}
+    it { should belong_to :author}
+  end
 end
