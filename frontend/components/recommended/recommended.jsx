d865c4fb2bf0a45b4a34b6ddf0055b4be783bc90
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-08 22:14:49 -0500

    Refactor Fetch logic

--- frontend/components/recommended/recommended.jsx
+++ frontend/components/recommended/recommended.jsx
@@ -9,31 +9,16 @@ class Recommended extends React.Component {
   }
 
   componentWillReceiveProps(nextProps){
-    if (this.props.followees.length !== nextProps.followees.length){
-      this.props.requestPotentialFollowees(nextProps.current_user.id);
+    if (nextProps.followees.length < 4 && this.props.followees.length >= 4){
+      this.props.requestPotentialFollowees(this.props.current_user.id);
     }
   }
 
-  shuffleList(arr){
-  let currIdx = arr.length, temp, randIdx;
-
-  while (0 !== currIdx) {
-    randIdx = Math.floor(Math.random() * currIdx);
-    currIdx -= 1;
-
-    temp = arr[currIdx];
-    arr[currIdx] = arr[randIdx];
-    arr[randIdx] = temp;
-  }
-  return arr;
-}
-
-
   render() {
-    let potentialList = this.shuffleList(this.props.followees).slice(0,4);
+    let potentialList = this.props.followees.slice(0,4);
       return (
       <div className = "follow-feed">
-        <span className="radar-title">Recommended Users</span>
+        <span className="radar-title">RECOMMENDED USERS</span>
         <ul>
           {potentialList.map((potential, idx) => <RecommendedItem key={idx} potential={potential} current_user={this.props.current_user} followUser={this.props.createFollow}/>) }
         </ul>
