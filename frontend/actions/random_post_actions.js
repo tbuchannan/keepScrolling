58bf7a553737968913b4d2db49a59e788fa4da60
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-14 23:32:18 -0500

    Random Post: Add actions

---
+++ frontend/actions/random_post_actions.js
@@ -0,0 +1,15 @@
+import * as RandomUtil from '../util/random_post_util';
+import { hashHistory } from 'react-router';
+
+
+export const RECEIVE_RANDOM_POST = 'RECEIVE_RANDOM_POST';
+
+export const receiveRandomPost = post => ({
+  type: RECEIVE_RANDOM_POST,
+  post
+});
+
+export const requestRandomPost = (id) => dispatch => (
+  RandomUtil.getRandomPost(id).then(
+    post => dispatch(receiveRandomPost(post)))
+);
