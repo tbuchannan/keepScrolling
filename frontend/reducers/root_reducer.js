226cabeb6355e73f5891c72442fca7aa95e12790
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-14 23:34:58 -0500

    Random Post: Add Slice of State

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import PostReducer from './post_reducer';
 import FollowReducer from './follow_reducer';
+import RandomPostReducer from './random_post_reducer';
 
 const rootReducer = combineReducers({
   session: SessionReducer,
   posts: PostReducer,
-  followees: FollowReducer
+  followees: FollowReducer,
+  randomPost: RandomPostReducer
 });
 
 export default rootReducer;
