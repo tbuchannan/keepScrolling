fa9d82dd6a522e61e25928a75244a366bdb928c6
Terence Buchannan <terencebuchannan@gmail.com>
2017-11-21 23:53:54 -0500

    PostContainer: Populate Followees slice earlier

--- frontend/components/post/post_container.js
+++ frontend/components/post/post_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import {requestAllPosts, requestOnePost, removeOnePost } from '../../actions/post_actions';
 import {selectAllPosts } from '../../reducers/selector';
+import { requestAllFollowees } from '../../actions/followee_actions.js';
 import PostIndex from './post_index';
 
 const mapStateToProps = (state, ownProps) => {
@@ -13,6 +14,7 @@ const mapDispatchToProps = (dispatch) => {
   return{
     requestOnePost: (id) => dispatch(requestOnePost(id)),
     requestAllPosts: (id) => dispatch(requestAllPosts(id)),
+    requestAllFollowees: (id) => dispatch(requestAllFollowees(id)),
     removeOnePost: (post) => dispatch(removeOnePost(post))
   };
 };
