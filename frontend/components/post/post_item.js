99252488f6e9d7a179ed33837500ca4eb50fa5c4
Terence Buchannan <terencebuchannan@gmail.com>
2018-02-20 20:41:16 -0500

    PostItem: Refactor behemoth logic

--- frontend/components/post/post_item.js
+++ frontend/components/post/post_item.js
@@ -1,140 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router';
+import LinkPost from '../indiv_posts/link_post.jsx';
+import AudioPost from '../indiv_posts/audio_post.jsx';
+import PhotoPost from '../indiv_posts/photo_post.jsx';
+import VideoPost from '../indiv_posts/video_post.jsx';
+import QuotePost from '../indiv_posts/quote_post.jsx';
+import TextPost from '../indiv_posts/text_post.jsx';
 
 const PostItem = (props) => {
-  if (!props.currentUser){return null;}
-  let currUserPost = "";
-  let editAvatar = "";
-  if (props.post.author === props.currentUser.id){
-    currUserPost = "curr";
-    editAvatar = "edit_avatar";
-  } else {
-     currUserPost = "hidden";
+  if (!props.currentUser){ return null; }
+  const isCurrUser = props.post.author === props.currentUser.id;
+  const currUserPost = isCurrUser ? "curr" : "hidden";
+  const editAvatar = isCurrUser ? "edit_avatar" : "";
+
+  let postBody;
+  switch (props.post.content) {
+    case "link":
+      postBody = <LinkPost post={ props.post }/>;
+      break;
+    case "audio":
+      postBody = <AudioPost post = { props.post } audio={ props.post.image_url }/>;
+      break;
+    case "photo":
+      postBody = <PhotoPost image = {props.post.image_url} post={ props.post }/>;
+      break;
+    case "video":
+      postBody = <VideoPost video={ props.post.image_url } post={ props.post } />;
+      break;
+    case "quote":
+      postBody = <QuotePost post = { props.post } />;
+      break;
+    default:
+      postBody = <TextPost post = { props.post }/>;
+      break;
   }
-  if (props.post.content === 'link'){
-    let externalLink = props.post.source.includes("://") ? props.post.source : 'https://' + props.post.source;
-    return (
-      <li className="li-post">
-      <div className={'user-avatar ' + editAvatar}>
-          <div className="user-image-stick">
-            <img src={props.post.avatar_url} />
-          </div>
-          <div className="edit_profile"><span className="change">Change</span></div>
-      </div>
-      <div className={"post"}>
-        <div className="post-header">
-          <span>{props.post.username}</span>
-        </div>
-        <div className="link-container">
-          <a className= "link-post-link" target="_blank" href={externalLink} >{props.post.source}
-            <div>
-              <div className="link-title">
-                <span>{props.post.title}
-                  <span className='huge-pointer'>  ></span>
-                </span>
-              </div>
-              <div className="link-summary">
-                <span>{props.post.summary}</span>
-              </div>
-            </div>
-          </a>
-        </div>
-          <div className = "indiv-post-description">
-            {props.post.body}
-          </div>
-          <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
-              </div>
-            </div>
-          </div>
-      </div>
-      </li>
-    );
-
-  }else if (props.post.content === 'audio') {
-    return(
-      <li className="li-post">
-        <div className={'user-avatar ' + editAvatar}>
-          <div className="user-image-stick">
-            <img src={props.post.avatar_url} />
-          </div>
-        </div>
-        <div className={"post"}>
-          <div className="post-header">
-            <span>{props.post.username}</span>
-          </div>
-          <div className="indiv-post-title">
-            <span>{props.post.title}</span>
-          </div>
-          <div className="indiv-post-body">
-
-            <div className="audio-post-audio">
-              <audio controls>
-                <source src={props.post.image_url} type="audio/mp3"></source>
-                <source src={props.post.image_url} type="audio/ogg"></source>
-                <source src={props.post.image_url} type="audio/wav"></source>
-              </audio>
-            </div>
-            <div className = "body-caption">
-              <span>{props.post.body}</span>
-            </div>
-          </div>
-          <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-    );
-  } else if (props.post.content === "photo"){
-
-    let picture = props.post.content === "photo" ? props.post.image_url : null;
-    return(
-      <li className="li-post">
-        <div className={'user-avatar ' + editAvatar}>
-          <div className="user-image-stick">
-            <img src={props.post.avatar_url} />
-          </div>
-        </div>
-        <div className={"post"}>
-          <div className="post-header">
-            <span>{props.post.username}</span>
-          </div>
-          <div className="indiv-post-title">
-            <span>{props.post.title}</span>
-          </div>
-          <div className="indiv-post-body">
-
-            <div className="picture-post-photo">
-              <img src={props.post.image_url} />
-            </div>
-            <div className = "body-caption">
-              <span>{props.post.body}</span>
-            </div>
-          </div>
-          <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </li>
-
-    );
-  } else if (props.post.content === "video") {
     return (
       <li className="li-post">
         <div className={'user-avatar ' + editAvatar}>
@@ -146,101 +45,18 @@ const PostItem = (props) => {
           <div className="post-header">
             <span>{props.post.username}</span>
           </div>
-          <div className="indiv-post-title">
-            <span>{props.post.title}</span>
-          </div>
-          <div className="indiv-video-body">
-            <div className="video-post-video">
-              <video width="540" height="360" controls>
-                <source src={props.post.image_url} type="video/mp4" />
-                <source src={props.post.image_url} type="video/ogg" />
-              </video>
-            </div>
-
-            <div className = "body-caption">
-              <span>{props.post.body}</span>
-            </div>
-          </div>
+          { postBody }
           <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
+              <div className={'cog-div ' + currUserPost}>
+                <i className="fa fa-cog" aria-hidden="true"></i>
+                <div className ="edit_delete_drop hidden">
+                  <button>Edit</button>
+                  <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
+                </div>
               </div>
             </div>
-          </div>
-          </div>
+        </div>
       </li>
     );
-  } else if (props.post.content === "quote"){
-    return (
-      <li className="li-post">
-        <div className={'user-avatar ' + editAvatar}>
-          <div className="user-image-stick">
-            <img src={props.post.avatar_url} />
-          </div>
-        </div>
-        <div className={"post"}>
-          <div className="post-header">
-            <span>{props.post.username}</span>
-          </div>
-            <div className="indiv-post-title">
-            <span>{props.post.body}</span>
-          </div>
-          <div className="indiv-post-body">
-
-            <div className = "body-caption">
-              <span>{props.post.title}</span>
-            </div>
-          </div>
-          <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
-              </div>
-            </div>
-          </div>
-          </div>
-        </li>
-    );
-  }
-    else {
-      return (
-      <li className="li-post">
-        <div className={'user-avatar ' + editAvatar}>
-          <div className="user-image-stick">
-            <img src={props.post.avatar_url} />
-          </div>
-        </div>
-        <div className={"post"}>
-          <div className="post-header">
-            <span>{props.post.username}</span>
-
-          </div>
-          <div className="indiv-post-title">
-            <span>{props.post.title}</span>
-          </div>
-          <div className="indiv-post-body">
-
-            <div className = "body-caption">
-              <span>{props.post.body}</span>
-            </div>
-          </div>
-          <div className="post-footer">
-            <div className={'cog-div ' + currUserPost}>
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <div className ="edit_delete_drop hidden">
-                <button>Edit</button>
-                <button onClick={() => props.removeOnePost(props.post.post_id)}>Delete</button>
-              </div>
-            </div>
-          </div>
-          </div>
-        </li>
-      );
-    }
   };
 export default PostItem;
